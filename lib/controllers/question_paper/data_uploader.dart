056a609265f5ab402b0572dfb4b5da23fdb826d2
Ali Waseem <ali.waseem27@gmail.com>
2022-08-28 00:11:36 +0300

    Adding the QuestionPaperModel and DataUploader functions

---
+++ lib/controllers/question_paper/data_uploader.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:sha5er/models/question_paper_model.dart';
+
+class DataUploader extends GetxController {
+  @override
+  void onReady() {
+    uploadData();
+    super.onReady();
+  }
+
+  Future<void> uploadData() async {
+    final manifestContent = await DefaultAssetBundle.of(Get.context!)
+        .loadString("AssetManifest.json");
+    final Map<String, dynamic> manifestMap = jsonDecode(manifestContent);
+    // Load and read json files in Assets
+    final papersInAssets = manifestMap.keys
+        .where((path) =>
+            path.startsWith("assets/DB/papers") && path.contains(".json"))
+        .toList();
+
+    List<QuestionPaperModel> questionPapers = [];
+    for (var paper in papersInAssets) {
+      String stringPaperContent = await rootBundle.loadString(paper);
+      questionPapers
+          .add(QuestionPaperModel.fromJson(json.decode(stringPaperContent)));
+    }
+    print("items number ${questionPapers[0].description}");
+  }
+}
